129f7feda6184f08fdb4a1b1f6daa1595c593904
Nasharius <mah_937@hotmail.com>
2018-10-25 11:19:22 -0400

    Travail original complete minus password matching

--- app/src/main/java/collegeshawinigan/ca/exercice5/MainActivity.java
+++ app/src/main/java/collegeshawinigan/ca/exercice5/MainActivity.java
@@ -1,19 +1,28 @@
 package collegeshawinigan.ca.exercice5;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context mContext = this;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = getIntent();
+        String message = intent.getStringExtra(SignUpActivity.EXTRA_MESSAGE);
+        Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
+
 
     }
+
+
 
     public void onClickSignUp(View v)
     {
--- app/src/main/java/collegeshawinigan/ca/exercice5/SignUpActivity.java
+++ app/src/main/java/collegeshawinigan/ca/exercice5/SignUpActivity.java
@@ -1,55 +1,89 @@
 package collegeshawinigan.ca.exercice5;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.exercice5.MESSAGE";
 
     TextInputEditText mTextEditPass;
     TextInputEditText mTextEditEmail;
     TextInputEditText mTextEditConfPass;
-
+    Button mSend;
+    Context mContext = this;
+    boolean mPass,mConf,mEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ImageView img = findViewById(R.id.imageView);
         img.setImageResource(R.drawable.index);
+
         mTextEditPass = findViewById(R.id.signup_edittextp);
         mTextEditEmail = findViewById(R.id.signup_email);
         mTextEditConfPass = findViewById(R.id.signup_edittextcp);
-        ValidatesFields(mTextEditPass, false);
-        ValidatesFields(mTextEditConfPass, false);
-        ValidatesFields(mTextEditEmail, true);
+        mPass = ValidatesFields(mTextEditPass, false);
+        mConf = ValidatesFields(mTextEditConfPass, false);
+        mEmail = ValidatesFields(mTextEditEmail, true);
+        mSend = findViewById(R.id.signup_send);
 
-
+        mSend.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Start NewActivity.class
+                mPass = ValidatesFields(mTextEditPass, false);
+                mConf = ValidatesFields(mTextEditConfPass, false);
+                mEmail = ValidatesFields(mTextEditEmail, true);
+                if(mPass && mEmail && mConf) {
+                    Intent myIntent = new Intent(mContext,
+                            MainActivity.class);
+                    myIntent.putExtra(EXTRA_MESSAGE,"Your account has been successfully created");
+                    startActivity(myIntent);
+                }else{
+                    Toast.makeText(mContext, "You have some errors in the above fields !", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    public void ValidatesFields(final TextInputEditText txt,final boolean email) {
+    public boolean ValidatesFields(final TextInputEditText txt,final boolean email) {
 
             txt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
                 @Override
                 public void onFocusChange(View v, boolean hasFocus) {
-                    if(!email) {
+                    if (!email) {
                         if (!hasFocus) {
                             if (txt.getText().toString().trim().length() < 8) {
                                 txt.setError("Minimum of 8 characters !");
+                            } else //if (txt.getText().toString() == mTextEditPass.getText().toString()) {
+                                txt.setError(null);
+
+                            /*}else if(!(txt.getText().toString() == mTextEditPass.getText().toString())){
+                                txt.setError("Password do not match !");
+                            }*/
+                        }
+                    } else if (email) {
+                        if (!hasFocus) {
+                            if (txt.getText().toString().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(txt.getText().toString()).matches()) {
+                                txt.setError("Enter a valid email address");
                             } else {
                                 txt.setError(null);
                             }
                         }
-                    } if(!hasFocus){
-                        if (txt.getText().toString().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(txt.getText().toString()).matches()) {
-                            txt.setError("Enter a valid email address");
-                        }
-                        else {
-                            txt.setError(null);
-                        }
                     }
                 }
             });
+
+            if (txt.getError() == null && !(txt.getText().toString().isEmpty())){
+                return true;
+            }else{
+                return false;
+            }
     }
 }
