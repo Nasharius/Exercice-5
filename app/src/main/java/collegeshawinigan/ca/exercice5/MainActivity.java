74afeaeb825ae068b5fb01a5ef82fc797615457e
Nasharius <mah_937@hotmail.com>
2018-10-25 10:01:06 -0400

    Validations are ok

--- app/src/main/java/collegeshawinigan/ca/exercice5/MainActivity.java
+++ app/src/main/java/collegeshawinigan/ca/exercice5/MainActivity.java
@@ -1,7 +1,10 @@
 package collegeshawinigan.ca.exercice5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClickSignUp(View v)
+    {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+        //Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/collegeshawinigan/ca/exercice5/SignUpActivity.java
+++ app/src/main/java/collegeshawinigan/ca/exercice5/SignUpActivity.java
@@ -1,13 +1,55 @@
 package collegeshawinigan.ca.exercice5;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    TextInputEditText mTextEditPass;
+    TextInputEditText mTextEditEmail;
+    TextInputEditText mTextEditConfPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ImageView img = findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.index);
+        mTextEditPass = findViewById(R.id.signup_edittextp);
+        mTextEditEmail = findViewById(R.id.signup_email);
+        mTextEditConfPass = findViewById(R.id.signup_edittextcp);
+        ValidatesFields(mTextEditPass, false);
+        ValidatesFields(mTextEditConfPass, false);
+        ValidatesFields(mTextEditEmail, true);
+
+
+    }
+
+    public void ValidatesFields(final TextInputEditText txt,final boolean email) {
+
+            txt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if(!email) {
+                        if (!hasFocus) {
+                            if (txt.getText().toString().trim().length() < 8) {
+                                txt.setError("Minimum of 8 characters !");
+                            } else {
+                                txt.setError(null);
+                            }
+                        }
+                    } if(!hasFocus){
+                        if (txt.getText().toString().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(txt.getText().toString()).matches()) {
+                            txt.setError("Enter a valid email address");
+                        }
+                        else {
+                            txt.setError(null);
+                        }
+                    }
+                }
+            });
     }
 }
