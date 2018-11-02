74d119e8e086a97feabff770b75a3c472bdfd79b
Nasharius <mah_937@hotmail.com>
2018-11-01 22:44:28 -0400

    Projet final -Master

--- app/src/main/java/collegeshawinigan/ca/exercice5/MainActivity.java
+++ app/src/main/java/collegeshawinigan/ca/exercice5/MainActivity.java
@@ -1,14 +1,19 @@
 package collegeshawinigan.ca.exercice5;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    static final int CODE = 1;
     Context mContext = this;
     Intent intent;
     @Override
@@ -17,18 +22,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         intent = getIntent();
         String message = intent.getStringExtra(SignUpActivity.EXTRA_MESSAGE);
-        Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
+        if(message != null) {
+            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
+        }
 
 
     }
-
-
 
     public void onClickSignUp(View v)
     {
         Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent,CODE);
         //Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+
+        if (requestCode == CODE) {
+            if (resultCode == RESULT_OK) {
+                ArrayList<Integer> mSelectedItems = data.getExtras().getIntegerArrayList("items");
+                String []mChoices = getResources().getStringArray(R.array.choices);
+                String []mResult = new String[mSelectedItems.size()];
+                for(int i = 0; i < mSelectedItems.size(); i++){
+                    mResult[i] = mChoices[mSelectedItems.get(i)];
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Themes").setItems(mResult, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+            }
+        }
+    }
 }
--- app/src/main/java/collegeshawinigan/ca/exercice5/SignUpActivity.java
+++ app/src/main/java/collegeshawinigan/ca/exercice5/SignUpActivity.java
@@ -1,8 +1,10 @@
 package collegeshawinigan.ca.exercice5;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,21 +12,30 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.Console;
+import java.util.ArrayList;
+
 public class SignUpActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.exercice5.MESSAGE";
 
     TextInputEditText mTextEditPass;
     TextInputEditText mTextEditEmail;
     TextInputEditText mTextEditConfPass;
+    ArrayList<String> mSelectedTheme;
+    static int mCount;
     Button mSend;
+    Button mCancel;
+    Toast showToast;
     Context mContext = this;
     boolean mPass,mConf,mEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Sign Up");
         setContentView(R.layout.activity_sign_up);
         ImageView img = findViewById(R.id.imageView);
         img.setImageResource(R.drawable.index);
+        final String mChoices[] = getResources().getStringArray(R.array.choices);
 
         mTextEditPass = findViewById(R.id.signup_edittextp);
         mTextEditEmail = findViewById(R.id.signup_email);
@@ -33,7 +44,13 @@ public class SignUpActivity extends AppCompatActivity {
         mConf = ValidatesFields(mTextEditConfPass, false);
         mEmail = ValidatesFields(mTextEditEmail, true);
         mSend = findViewById(R.id.signup_send);
-
+        mCancel = findViewById(R.id.signup_cancel);
+        showToast = new Toast(SignUpActivity.this);
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
         mSend.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
                 // Start NewActivity.class
@@ -41,10 +58,45 @@ public class SignUpActivity extends AppCompatActivity {
                 mConf = ValidatesFields(mTextEditConfPass, false);
                 mEmail = ValidatesFields(mTextEditEmail, true);
                 if(mPass && mEmail && mConf) {
-                    Intent myIntent = new Intent(mContext,
-                            MainActivity.class);
-                    myIntent.putExtra(EXTRA_MESSAGE,"Your account has been successfully created");
-                    startActivity(myIntent);
+
+                   final ArrayList<Integer> mSelectedItem = new ArrayList<Integer>();
+
+                    mCount = 0;
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+
+                    builder.setTitle("Choose a theme")
+                    .setMultiChoiceItems(mChoices, null, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id, boolean isChecked) {
+                            if( isChecked){
+                                if(mCount <= 3)
+                                    mSelectedItem.add(id);
+                                    mCount++;
+                            }else if(mSelectedItem.contains(id)){
+
+                                    mSelectedItem.remove(Integer.valueOf(id));
+                                    mCount--;
+                            }
+
+                        }
+                    }).setPositiveButton("Save themes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            String selectedIndex = "";
+                            for(Integer i : mSelectedItem){
+                                selectedIndex += i + ", ";
+                                System.out.println("ICI " + selectedIndex + mSelectedItem.size());
+                            }
+                            Intent resultIntent = new Intent();
+                            resultIntent.putExtra("items", mSelectedItem);
+                            setResult(RESULT_OK, resultIntent);
+                            finish();
+                        }
+                    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                        }
+                    }).show();
                 }else{
                     Toast.makeText(mContext, "You have some errors in the above fields !", Toast.LENGTH_LONG).show();
                 }
